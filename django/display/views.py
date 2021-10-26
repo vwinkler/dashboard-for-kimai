e605c822fc7f65d12819e3ea17bfdd58720044c7
Vincent Winkler <vincent.winkler@student.kit.edu>
2021-10-26 13:03:13 +0200

    Implement dashboard that shows active kimai records of a fixed user

--- django/display/views.py
+++ django/display/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from decouple import config
+import requests
 
 def index(request):
-    print("TEST")
-    return render(request, "display/index.html", dict())
+
+    headers = {
+            'X-AUTH-USER': config('KIMAI_X-AUTH-USER'),
+            'X-AUTH-TOKEN': config('KIMAI_X-AUTH-TOKEN')
+            }
+    r = requests.get('http://' + config('KIMAI_HOST') + '/api/timesheets/active', headers=headers)
+    r.raise_for_status()
+
+    return render(request, "display/index.html", {"records": r.json()})
